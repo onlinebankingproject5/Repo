aa019f88980497458c144255513d19f7a0cae5c5
onlinebankingproject5 <54817277+onlinebankingproject5@users.noreply.github.com>
2019-09-15 18:41:21 +0530

    DDL schema and SQL commands for Oracle DB

---
+++ export.sql
@@ -0,0 +1,7 @@
+REM INSERTING into EXPORT_TABLE
+
+create table Branch(BRANCHID number,CITY varchar2(20),IFSCCODE varchar2(20),STATE varchar2(20),BRANCH varchar2(20),CONTACTNO number);
+Insert into Branch (BRANCHID,CITY,IFSCCODE,STATE,BRANCH,CONTACTNO) values (111,'HYD','HYD111','Telangana','Balanagar',14007714);
+Insert into Branch (BRANCHID,CITY,IFSCCODE,STATE,BRANCH,CONTACTNO) values (112,'BNGLR','BNGLR112','Karnakata','Hebbal',14220121);
+Insert into Branch (BRANCHID,CITY,IFSCCODE,STATE,BRANCH,CONTACTNO) values (113,'DEL','DEL113','Delhi','Gandhi nagar',1452362);
+Insert into Branch (BRANCHID,CITY,IFSCCODE,STATE,BRANCH,CONTACTNO) values (114,'KOL','KOL114','West Bengal','Burra Bazar',146556552);
